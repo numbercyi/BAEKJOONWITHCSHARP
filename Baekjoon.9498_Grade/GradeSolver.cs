e99350147fcdd96adb468f062ed2de1474122fdb
yichoi <yichoi@bowoosystem.com>
2025-05-12 12:58:44 +0900

    20250412

---
+++ Baekjoon.9498_Grade/GradeSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Baekjoon.Common;
+
+namespace Baekjoon._9498_Grade
+{
+    public class GradeSolver
+    {
+        public void Run()
+        {
+            String[] input = Console.ReadLine()!.Split();
+            int a = int.Parse(input[0]);
+
+            switch (a)
+            {
+                case >= 90:
+                    Console.WriteLine("A");
+                    break;
+                case >= 80:
+                    Console.WriteLine("B");
+                    break;
+                case >= 70:
+                    Console.WriteLine("C");
+                    break;
+                case >= 60:
+                    Console.WriteLine("D");
+                    break;
+                default:
+                    Console.WriteLine("F");
+                    break;
+            }
+        }
+    }
+}
---
+++ Baekjoon.9498_Grade/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Baekjoon._9498_Grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new GradeSolver();
+            solver.Run();
+        }
+    }
+}
